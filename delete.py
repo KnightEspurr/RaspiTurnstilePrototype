6e30e251e2e964936b7eb3432fc95b01fefdba6f
Joel Goh <joelgohym@gmail.com>
2022-04-02 22:34:26 +0800

    Delete delete.py

--- delete.py
+++
@@ -1,8 +0,0 @@
-import time
-from datetime import datetime, timedelta
-from allmyfunctions import *
-print("Delete scheduler activated! This operation will repeat every 15 minutes")
-while True:
-    print(str(datetime.now()))
-    deleteExpiredRecords()
-    time.sleep(interval)
