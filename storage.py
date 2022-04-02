73f3739b856d3fa4c7bcb157449d2b0dc0a192cb
Joel Goh <joelgohym@gmail.com>
2022-04-02 22:35:38 +0800

    Update storage.py

--- storage.py
+++ storage.py
@@ -5,19 +5,6 @@ from PIL import ImageTk,Image
 import threading
 from allmyfunctions import *
 
-root = tk.Tk()
-root.title('Magic Mirror')
-root.geometry("250x250")
-
-#get the 3 images
-defaultimg = ImageTk.PhotoImage(Image.open("img/blank.png").resize((175, 175), Image.ANTIALIAS))
-successimg = ImageTk.PhotoImage(Image.open("img/proceed.jpg").resize((175, 175), Image.ANTIALIAS))
-errorimg = ImageTk.PhotoImage(Image.open("img/red-stop.jpg").resize((175, 175), Image.ANTIALIAS))
-
-#image
-imglabel = tk.Label(root, image=defaultimg)
-imglabel.pack()
-
 def subscribeVerify2(client: mqtt_client):
     def on_message(client, userdata, msg):
         with open(storage, 'r') as file:
@@ -31,22 +18,16 @@ def subscribeVerify2(client: mqtt_client):
                     "timestamp": str(datetime.now())
                 }
                 writeJson(dictionary)
-
-                imglabel.config(image=successimg)
-                threading.Timer(3, resetImg).start()
             else:
                 print(f"{msg.payload.decode()} already scanned")
-                imglabel.config(image=errorimg)
-                threading.Timer(3, resetImg).start()
     client.subscribe(topic)
     client.on_message = on_message
-
-def resetImg():
-    imglabel.config(image=defaultimg)
 
 client = connect_mqtt_receive()
 subscribeVerify2(client)
 #client.loop_forever()
 client.loop_start()
-
-root.mainloop()
+while True:
+    print(str(datetime.now()))
+    deleteExpiredRecords()
+    time.sleep(300)
